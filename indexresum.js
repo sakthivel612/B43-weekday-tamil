55da57d044976ceb49aa6999845d4fb2dd4d618f
sakthivel612 <sv69811@gmail.com>
2024-01-21 20:32:00 -0800

    all_commit

--- indexresum.js
+++ indexresum.js
@@ -1,10 +1,12 @@
-var Resume = [{
-    "Name" : "Sakthivel.k",
-    "Age" : "22",
-    "Phone_number" : "+91 ---",
-    "Gender" : "Male",
-    "Email" : "---@gmail.com",
-    "Qualifications" : "Msc(Computer science)",
-    "Languages" : "Tamil,English",
-    "Address" : "------"
-}]
+var Resume = [
+  {
+    Name: "Sakthivel.k",
+    Age: "22",
+    Phone_number: "+91 ---",
+    Gender: "Male",
+    Email: "---@gmail.com",
+    Qualifications: "Msc(Computer science)",
+    Languages: "Tamil,English",
+    Address: "------",
+  },
+];
